262a82a010ed90f892bffd80da074d24bd43e711
OUALY Ossama <ossamaoualy@gmail.com>
2025-12-07 19:08:14 +0100

    update core/ml/predictor

--- core/ml/predictor.py
+++ core/ml/predictor.py
@@ -1,2 +1,20 @@
 def predict(model, features):
-    raise NotImplementedError
+    """
+    Prédit l'instrument à partir des features
+
+    Args:
+        model : modèle ML
+        features : caractéristiques extraites du fichier audio
+
+    Return:
+        Dict : instrument et confidence
+    """
+    
+    
+    return  {
+        "instrument": instrument,
+        "confidence": confidence
+    }
+
+    
+    # raise NotImplementedError
