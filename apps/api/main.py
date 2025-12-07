ec813a11fe2cdc9f9071e5f68ba3ab57b5f3e04c
OUALY Ossama <ossamaoualy@gmail.com>
2025-12-07 18:11:29 +0100

    update api part

--- apps/api/main.py
+++ apps/api/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
-app = FastAPI(title="InstruSense API")
+from routers import prediction
+
+# L'application FastAPI
+app = FastAPI(title="InstruSense API",
+    description="API de reconnaissance d'instruments")
+
+# les routes
+app.include_router(prediction.router)
 
 @app.get("/health")
 def health():
--- apps/api/routers/prediction.py
+++ apps/api/routers/prediction.py
@@ -1,7 +1,14 @@
 from fastapi import APIRouter
+from typing import Dict
+
+from apps.api.dto.prediction import PredictIn
 
 router = APIRouter()
 
 @router.post("/predict")
-def predict():
+async def predict(input_data: PredictIn)-> Dict:  # convertir les données en objet PredictIn (comme avner a fait)
+    # Récupérer les données de la requête
+    # appeler le service de prédiction
+    # convertir la réponse en objet PredictOut
+    # retourner la réponse
     return {"detail": "endpoint predict placeholder"}
