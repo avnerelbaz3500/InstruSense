579cb23f291cee29531cbf088ebf8d42439b1b03
OUALY Ossama <ossamaoualy@gmail.com>
2025-12-07 19:03:17 +0100

    update core/audio/analyze

--- core/audio/analyzer.py
+++ core/audio/analyzer.py
@@ -11,10 +11,26 @@ def extract_features(waveform, sr):
         sr       : Fréquence d'échantillonnage 
 
     Return:
+        Dictionnaire de caractéristiques audio extraites
         
     """
+    mfccs = librosa.feature.mfcc(y=waveform, sr=sr, n_mfcc=13)
+    mfccs_mean = np.mean(mfccs, axis=1)
+
+    # chroma
+    chroma = librosa.feature.chroma_stft(y=waveform, sr=sr)
+    chroma_mean = np.mean(chroma, axis=1)
     
+    #spectrogramme Mel
+    mel = librosa.feature.melspectrogram(y=waveform, sr=sr)
+    mel_mean = np.mean(mel, axis=1)
+
     
+    features = {
+        'mfccs': mfccs_mean,
+        'chroma': chroma_mean,
+        'mel': mel_mean
+    }
     
     return features
     # raise NotImplementedError
