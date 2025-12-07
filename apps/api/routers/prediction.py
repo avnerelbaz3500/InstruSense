a7c511b7df0c647782c8c021936d7a258b56244a
OUALY Ossama <ossamaoualy@gmail.com>
2025-12-07 18:21:25 +0100

    specify the output of predict(route)

--- apps/api/routers/prediction.py
+++ apps/api/routers/prediction.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter
 from typing import Dict
 
-from apps.api.dto.prediction import PredictIn
+from apps.api.dto.prediction import PredictIn, PredictOut
 
 router = APIRouter()
 
-@router.post("/predict")
-async def predict(input_data: PredictIn)-> Dict:  # convertir les données en objet PredictIn (comme avner a fait)
+@router.post("/predict", response_model=PredictOut)
+async def predict(input_data: PredictIn)-> PredictOut:  # convertir les données en objet PredictIn (comme avner a fait)
     # Récupérer les données de la requête
     # appeler le service de prédiction
     # convertir la réponse en objet PredictOut
